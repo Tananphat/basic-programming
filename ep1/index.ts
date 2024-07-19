8a20e6aceb2a8341afebc223152feb4054116c9c
Tananphat <tananphat2547@gmail.com>
2024-07-19 16:12:17 +0700

    Update index.ts

--- ep1/index.ts
+++ ep1/index.ts
@@ -6,3 +6,12 @@ let อยากกลับบ้าน:boolean=false
 console.log(Y)
 console.log(firstName)
 console.log(อยากกลับบ้าน)
+
+const a: number =1
+const b: number =2
+
+console.log(a==b)
+
+console.log(a>b)
+
+console.log(a<=b)

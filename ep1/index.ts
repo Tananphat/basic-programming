17fa00e416fa0858fc9faffc68d4350f91bc94bf
Tananphat <tananphat2547@gmail.com>
2024-07-19 16:28:43 +0700

    Update index.ts

--- ep1/index.ts
+++ ep1/index.ts
@@ -1,11 +1,11 @@
 const firstName: string='Tananaphat'
 let Y :number = 10
 Y = 20
-let อยากกลับบ้าน:boolean=false
+let isComeHome:boolean=false
 
 console.log(Y)
 console.log(firstName)
-console.log(อยากกลับบ้าน)
+console.log(isComeHome)
 
 const a: number =1
 const b: number =2

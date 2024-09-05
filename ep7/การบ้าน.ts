521ceb817c9d48bcb9849fbbb26605f1e4fbeb3c
Tananphat <tananphat2547@gmail.com>
2024-09-05 20:56:27 +0700

    การบ้าน

---
+++ ep7/การบ้าน.ts
@@ -0,0 +1,14 @@
+const perSoNalInForMation  = {
+    fullName: 'Tananphat TaengOn',
+    adDress : '87/6' ,
+    sWine : '8',
+    road : 'Srinakarin'
+    disTrict : 'Some cities',
+    canTon : 'cities',
+    ciTy : 'Samut Prakan'
+    zipCode :'50498'
+    eMail :'yoHo@gmail.com'
+
+}
+
+console.log(perSoNalInForMation)
